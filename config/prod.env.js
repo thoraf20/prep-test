97c154c3202d1092a9b1a392e7a18edb5362a8c0
Obodugo Rapheal <obodugorapheal@gmail.com>
2019-01-18 13:42:43 +0100

    Updated API url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 const BASE_API = "https://api-test.prepclassng.com";
-const PUBLIC_URL = "https://admin-test.prepclassng.com";
+const PUBLIC_URL = "https://api-test.prepclassng.com";
 const PUSHER_APP_KEY = '23b7275c0bbb5e4aa24b';
 
 module.exports = {
