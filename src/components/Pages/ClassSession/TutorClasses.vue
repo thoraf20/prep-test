a95485ceef391e597066c45e76defe5b5cd84156
Obodugo Rapheal <obodugorapheal@gmail.com>
2019-02-01 10:44:46 +0100

    Changed button style

--- src/components/Pages/ClassSession/TutorClasses.vue
+++ src/components/Pages/ClassSession/TutorClasses.vue
@@ -48,10 +48,20 @@
               </td>
               <td>{{ c.tutor !== null && c.tutor.phone !== null ? c.tutor.phone : 'Not Available' }}</td>
               <td>
-                <router-link :to="{ name: 'tutor-class', params: { id: c.id }}">
+                <div class="row">
+                  <div class="col-sm-6 col-lg-6">
+                     <router-link :to="{ name: 'tutor-class', params: { id: c.id }}">
+                      <button class="btn btn-sm btn-sml btn-primary">View Class </button>
+                    </router-link>
+                  </div>
+                  <div class="col-sm-6 col-lg-6">
+                     <button @click.prevent="showModal(c.id)" class="btn btn-sm btn-danger btn-td">Make Complaint </button>
+                  </div>
+                </div>
+                <!-- <router-link :to="{ name: 'tutor-class', params: { id: c.id }}">
                   <button class="btn btn-sm btn-sml btn-primary">View Class </button>
                 </router-link>
-                <button @click.prevent="showModal(c.id)" class="btn btn-sm btn-danger btn-td">Make Complaint </button>
+                <button @click.prevent="showModal(c.id)" class="btn btn-sm btn-danger btn-td">Make Complaint </button> -->
                 <!-- <b-dropdown variant="link" size="lg" no-caret id="dsdag">
                   <template slot="button-content">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="25px" height="25px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve" class=""><g><g><g><circle cx="69.545" cy="306" r="69.545" data-original="#000000" class="active-path" data-old_color="#000000" fill="#3B86FF"/><circle cx="306" cy="306" r="69.545" data-original="#000000" class="active-path" data-old_color="#000000" fill="#3B86FF"/><circle cx="542.455" cy="306" r="69.545" data-original="#000000" class="active-path" data-old_color="#000000" fill="#3B86FF"/></g></g></g> </svg>
